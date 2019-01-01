ae32c90b79d1af3daa5f17af963a7454bc296f4e
vitaliks <vitaly@anagorsky.com>
2019-01-01 21:45:06 +0300

    0.0.1

---
+++ src/index.js
@@ -0,0 +1,336 @@
+
+/**
+ * Native
+ */
+
+const net = require('net')
+const util = require('util')
+const events = require('events')
+
+/**
+ * Jiu-Jitsu
+ */
+
+const ___error = require('jiu-jitsu-error')
+
+/**
+ * Protocol
+ */
+
+const ___protocol = require('./protocol')
+
+/**
+ * Mongo class
+ */
+
+class Mongo extends events {
+
+	constructor (endpoint) {
+
+		super()
+
+		this.___id = 1
+		this.___buffers = []
+		this.___callbacks = {}
+		this.___timeout = 1000
+		this.___connected = false
+		this.___reconnecting = false
+		this.___endpoint = endpoint
+		this.___connect()
+
+		/**
+		 * Workaround
+		 */
+
+		this.find = util.promisify(this.find)
+		this.insert = util.promisify(this.insert)
+		this.update = util.promisify(this.update)
+		this.remove = util.promisify(this.remove)
+		this.aggregate = util.promisify(this.aggregate)
+
+	}
+
+	___connect () {
+
+		this.___socket = new net.Socket()
+		this.___socket
+
+			.on('connect', (error) => this.___onConnect(error))
+			.on('error', (error) => this.___onError(error))
+			.on('data', (data) => this.___onData(data))
+			.on('end', (error) => this.___onEnd(error))
+			.connect(this.___endpoint)
+
+	}
+
+	___reconnect (error) {
+
+		const ids = Object.keys(this.___callbacks)
+
+		ids.forEach((id) => this.___callbacks[id] && this.___callbacks[id](___error('jiu-jitsu-mongo/MONGO_CONNECTION_HAS_BEEN_CLOSED', error)))
+
+		this.___buffers = []
+		this.___callbacks = {}
+		this.___reconnecting = true
+		this.___socket.___protocol.flush()
+
+		setTimeout(() => this.___connect(), this.___timeout)
+
+	}
+
+	___protocol () {
+
+		this.___socket.___protocol = new ___protocol()
+		this.___socket.___protocol.on('message', (message) => this.___onMessage(message))
+
+	}
+
+	___onConnect (error) {
+
+		this.___protocol()
+		this.___reconnecting = false
+
+		if (!this.___connected) {
+
+			this.___connected = true
+			process.nextTick(() => this.emit('ready'))
+
+		}
+
+	}
+
+	___onError (error) {
+
+		this.___reconnect(error)
+
+	}
+
+	___onData (chunk) {
+
+		this.___socket.___protocol.read(chunk)
+
+	}
+
+	___onEnd (error) {
+
+		this.___reconnect()
+
+	}
+
+	___onMessage (message) {
+
+		const callback = this.___callbacks[message.id]
+
+		if (!callback) {
+
+			return
+
+		} else {
+
+			delete this.___callbacks[message.id]
+
+		}
+
+		return callback(message.error, message.data) | this.___write()
+
+	}
+
+	___write () {
+
+		const len = this.___buffers.length
+
+		if (!len) {
+
+			return
+
+		}
+
+		const buffer = Buffer.concat(this.___buffers)
+
+		this.___buffers = []
+		this.___socket.write(buffer)
+
+	}
+
+	find (message, callback) {
+
+		message = message || {}
+
+		if (!this.___connected || this.___reconnecting) {
+
+			return callback(___error('jiu-jitsu-mongo/MONGO_SOCKET_IS_NOT_READY'))
+
+		}
+
+		const id = this.___id++
+		const db = this.___endpoint.db
+
+		const ___message = {}
+		const ___options = {}
+
+		/**
+		 * x - default collection name
+		 */
+
+		___message.find = 'x'
+		___message.$db = db
+		___message.filter = message.filter || {}
+		___message.sort = message.sort || {}
+		___message.limit = message.limit || 999999999
+		___message.batchSize = message.limit || 999999999
+		___message.projection = message.projection || {}
+		___message.projection._id = 0
+
+		___options.id = id
+		___options.db = db
+
+		const buffer = this.___socket.___protocol.write(___message, ___options)
+
+		this.___buffers.push(buffer)
+		this.___callbacks[id] = (error, data) => callback(error, data)
+		this.___write()
+
+	}
+
+	insert (message, callback) {
+
+		if (!this.___connected || this.___reconnecting) {
+
+			return callback(___error('jiu-jitsu-mongo/MONGO_SOCKET_IS_NOT_READY'))
+
+		}
+
+		const id = this.___id++
+		const db = this.___endpoint.db
+
+		const ___message = {}
+		const ___options = {}
+
+		___message.insert = 'x'
+		___message.$db = db
+		___message.documents = []
+		___message.documents.push(message)
+		___message.ordered = true
+
+		___options.id = id
+		___options.db = db
+
+		const buffer = this.___socket.___protocol.write(___message, ___options)
+
+		this.___buffers.push(buffer)
+		this.___callbacks[id] = (error) => callback(error, message)
+		this.___write()
+
+	}
+
+	update (message, callback) {
+
+		if (!this.___connected || this.___reconnecting) {
+
+			return callback(___error('jiu-jitsu-mongo/MONGO_SOCKET_IS_NOT_READY'))
+
+		}
+
+		const id = this.___id++
+		const db = this.___endpoint.db
+
+		const ___message = {}
+		const ___options = {}
+
+		___message.update = 'x'
+		___message.$db = db
+		___message.updates = []
+		___message.updates[0] = {}
+		___message.updates[0].q = message.filter || {}
+		___message.updates[0].u = message.update || {}
+		___message.updates[0].multi = !!message.multi || false
+		___message.updates[0].upsert = !!message.upsert || false
+
+		___options.id = id
+		___options.db = db
+
+		const buffer = this.___socket.___protocol.write(___message, ___options)
+
+		this.___buffers.push(buffer)
+		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok && data.n || 0)
+		this.___write()
+
+	}
+
+	remove (message, callback) {
+
+		if (!this.___connected || this.___reconnecting) {
+
+			return callback(___error('jiu-jitsu-mongo/MONGO_SOCKET_IS_NOT_READY'))
+
+		}
+
+		const id = this.___id++
+		const db = this.___endpoint.db
+
+		const ___message = {}
+		const ___options = {}
+
+		/**
+		 * limit - specify either a 0 to delete all matching documents, or 1 to delete a single document
+		 */
+
+		___message.delete = 'x'
+		___message.$db = db
+		___message.deletes = []
+		___message.deletes[0] = {}
+		___message.deletes[0].q = message.filter || {}
+		___message.deletes[0].limit = !message.multi && 1 || 0
+
+		___options.id = id
+		___options.db = db
+
+		const buffer = this.___socket.___protocol.write(___message, ___options)
+
+		this.___buffers.push(buffer)
+		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok && data.n || 0)
+		this.___write()
+
+	}
+
+	aggregate (message, callback) {
+
+		if (!this.___connected || this.___reconnecting) {
+
+			return callback(___error('jiu-jitsu-mongo/MONGO_SOCKET_IS_NOT_READY'))
+
+		}
+
+		const id = this.___id++
+		const db = this.___endpoint.db
+
+		const ___message = {}
+		const ___options = {}
+
+		___message.aggregate = 'x'
+		___message.$db = db
+		___message.pipeline = []
+		___message.pipeline = message.pipeline
+		___message.cursor = {}
+		___message.cursor.batchSize = message.limit || 999999999
+
+		___options.id = id
+		___options.db = db
+
+		const buffer = this.___socket.___protocol.write(___message, ___options)
+
+		this.___buffers.push(buffer)
+		this.___callbacks[id] = (error, data) => callback(error, !error && data || null)
+		this.___write()
+
+	}
+
+}
+
+/**
+ * Export
+ */
+
+module.exports = Mongo
+
+
---
+++ src/protocol.js
@@ -0,0 +1,267 @@
+
+/**
+ * Native
+ */
+
+const zlib = require('zlib')
+const events = require('events')
+
+/**
+ * Jiu-Jitsu
+ */
+
+const ___bson = require('jiu-jitsu-bson')
+
+/**
+ * Constants
+ */
+
+const OP_MSG = 2013
+const OP_COMPRESSED = 2012
+const COMPRESSOR_ID_ZLIB = 2
+
+/**
+ * This driver implement Modern Wire Protocol 3.6
+ *
+ * https://docs.mongodb.com/manual/reference/mongodb-wire-protocol
+ * https://github.com/mongodb/specifications/blob/master/source/message/OP_MSG.rst
+ * https://github.com/mongodb/specifications/blob/master/source/compression/OP_COMPRESSED.rst
+ */
+
+class Protocol extends events {
+
+	constructor () {
+
+		super()
+
+		this.___read = {}
+		this.___read.buffer = null
+		this.___read.buffers = []
+		this.___read.length = 0
+
+	}
+
+	read (chunk) {
+
+		if (chunk) {
+
+			this.___read.length += chunk.length
+			this.___read.buffers.push(chunk)
+
+		}
+
+		if (!this.___read.buffers[0]) {
+
+			return
+
+		}
+
+		if (!this.___read.buffers[0].length) {
+
+			return
+
+		}
+
+		const header_length = this.___read.buffers[0].readInt32LE(0)
+
+		if (this.___read.length < header_length) {
+
+			return
+
+		}
+
+		/**
+		 * Concat completely received data
+		 */
+
+		this.___read.buffer = Buffer.concat(this.___read.buffers)
+
+		/**
+		 * Parse compressed message
+		 */
+
+		const next = this.___read.buffer.slice(0, header_length)
+
+		const header_message_length = next.readInt32LE(0)
+		const header_req_id = next.readInt32LE(4)
+		const header_res_id = next.readInt32LE(8)
+		const header_op_code = next.readInt32LE(12)
+
+		const original_op_code = next.readInt32LE(16)
+		const uncompressed_size = next.readInt32LE(20)
+		const compressor_id = next.readUInt8(24)
+
+		const compressed = next.slice(25)
+		const decompressed = zlib.unzipSync(compressed)
+
+		const body = ___bson.read(decompressed.slice(5))
+
+		const message = {
+			id: null,
+			error: null,
+			data: null
+		}
+
+		message.id = header_res_id
+
+		if (body.writeErrors) {
+
+			message.error = body.writeErrors[0]
+
+		} else if (body.errmsg) {
+
+			message.error = body
+
+		} else {
+
+			message.data = body.cursor && body.cursor.firstBatch || body.value || body
+
+		}
+
+		this.___read.buffer = this.___read.buffer.slice(header_length, this.___read.length)
+		this.___read.buffers = this.___read.buffer.length ? [this.___read.buffer] : []
+		this.___read.length = this.___read.buffer.length
+
+		this.emit('message', message)
+		this.read()
+
+	}
+
+	write (message, options) {
+
+		/**
+		 * Bson
+		 */
+
+		const buffer_message = ___bson.write(message)
+
+		/**
+		 * Uncompressed
+		 */
+
+		const buffer = Buffer.alloc(
+
+			/**
+			 * header
+			 *
+			 * int32 - message Length
+			 * int32 - req id
+			 * int32 - res id
+			 * int32 - op code
+			 */
+
+			4 * 4
+
+			/**
+			 * flagBits
+			 */
+
+			+ 4
+
+			/**
+			 * payloadType 0
+			 */
+
+			+ 1
+
+			/**
+			 * document
+			 */
+
+			+ buffer_message.length
+
+		)
+
+		buffer.writeInt32LE(buffer.length, 0)
+		buffer.writeInt32LE(options.id, 4)
+		buffer.writeInt32LE(0, 8)
+		buffer.writeInt32LE(OP_MSG, 12)
+		buffer.writeInt32LE(0, 16)
+		buffer.writeUInt8(0, 20)
+		buffer_message.copy(buffer, 21)
+
+		return this.___writeZip(buffer, options)
+
+	}
+
+	___writeZip (decompressed, options) {
+
+		/**
+		 * Skip header (16 bytes)
+		 */
+
+		const to_be_compressed = decompressed.slice(16)
+
+		/**
+		 * Compress
+		 */
+
+		const compressed = zlib.deflateSync(to_be_compressed)
+
+		/**
+		 * Full compressed buffer
+		 */
+
+		const buffer = Buffer.alloc(
+
+			/**
+			 * header
+			 *
+			 * int32 - message Length
+			 * int32 - req id
+			 * int32 - res id
+			 * int32 - op code
+			 */
+
+			4 * 4
+
+			/**
+			 * int32 - original op code
+			 */
+
+			+ 4
+
+			/**
+			 * int32 - uncompressed size (the size of decompressed message, which excludes the header)
+			 */
+
+			+ 4
+
+			/**
+			 * uint8 - compressor id
+			 */
+
+			+ 1
+
+			/**
+			 * compressed data
+			 */
+
+			+ compressed.length
+
+		)
+
+		buffer.writeInt32LE(16 + 9 + compressed.length, 0)
+		buffer.writeInt32LE(options.id, 4)
+		buffer.writeInt32LE(0, 8)
+		buffer.writeInt32LE(OP_COMPRESSED, 12)
+
+		buffer.writeInt32LE(decompressed.readInt32LE(12), 16)
+		buffer.writeInt32LE(to_be_compressed.length, 20)
+		buffer.writeUInt8(COMPRESSOR_ID_ZLIB, 24)
+
+		compressed.copy(buffer, 25)
+
+		return buffer
+
+	}
+
+}
+
+/**
+ * Return
+ */
+
+module.exports = Protocol
+
+

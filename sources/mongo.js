ae64aafdedd3aecaf78f1e36b9a33aa47151518f
Vitaly Anagorsky <vitaly@anagorsky.com>
2020-09-27 14:04:45 +0300

    0.0.1

--- sources/mongo.js
+++ sources/mongo.js
@@ -4,14 +4,12 @@
  */
 
 const net = require(`net`)
-const util = require(`util`)
-const events = require(`events`)
 
 /**
  *
  */
 
-const ___error = require(`jiu-jitsu-error`)
+const ___log = require(`jiu-jitsu-log`)
 
 /**
  *
@@ -23,800 +21,279 @@ const ___protocol = require(`./protocol`)
  *
  */
 
-class Mongo extends events {
+class Mongo {
 
 	/**
 	 *
 	 */
 
-	constructor (endpoint) {
-
-		/**
-		 *
-		 */
-
-		super()
-
-		/**
-		 *
-		 */
-
+	constructor (options) {
 		this.___id = 1
-		this.___callbacks = {}
-		this.___timeout = 1000
-		this.___connected = false
-		this.___reconnecting = false
-		this.___endpoint = endpoint
-		this.___connect()
-
-		/**
-		 *
-		 */
-
-		this.find = util.promisify(this.find)
-		this.insert = util.promisify(this.insert)
-		this.update = util.promisify(this.update)
-		this.remove = util.promisify(this.remove)
-		this.aggregate = util.promisify(this.aggregate)
-		this.removeIndex = util.promisify(this.removeIndex)
-		this.createIndex = util.promisify(this.createIndex)
-		this.getIndexes = util.promisify(this.getIndexes)
-
+		this.___socket = null
+		this.___protocol = null
+		this.___options = options
+		this.___promises = {}
 	}
 
 	/**
 	 *
 	 */
 
-	___protocol () {
-
-		/**
-		 *
-		 */
-
-		this.___socket.___protocol = new ___protocol()
-		this.___socket.___protocol.on(`message`, (message) => this.___onMessage(message))
-
+	async connect () {
+		await new Promise((resolve) => this.___connect(resolve))
 	}
 
 	/**
 	 *
 	 */
 
-	___connect () {
-
-		/**
-		 *
-		 */
-
+	___connect (resolve) {
+		const options = this.___options
 		this.___socket = new net.Socket()
-		this.___socket
-
-			.on(`connect`, (error) => this.___onConnect(error))
-			.on(`error`, (error) => this.___onError(error))
-			.on(`data`, (data) => this.___onData(data))
-			.on(`end`, (error) => this.___onEnd(error))
-			.connect(this.___endpoint)
-
+		this.___protocol = new ___protocol()
+		this.___protocol.on(`message`, (message) => this.___onProtocolMessage(message))
+		this.___socket.on(`connect`, (error) => this.___onSocketConnect(error, resolve))
+		this.___socket.on(`error`, (error) => this.___onSocketError(error))
+		this.___socket.on(`data`, (data) => this.___onSocketData(data))
+		this.___socket.connect(options)
 	}
 
 	/**
 	 *
 	 */
 
-	___reconnect (error) {
+	___onSocketConnect (error, resolve) {
+		const options = this.___options
+		___log(`jiu-jitsu-mongo`, `OK`, `${options.db} ✔`)
+		resolve(error)
+	}
 
-		/**
-		 *
-		 */
+	/**
+	 *
+	 */
 
-		const ids = Object.keys(this.___callbacks)
+	___onSocketError (error) {
+		const options = this.___options
+		___log(`jiu-jitsu-mongo`, `FAIL`, `${options.db} !`, error, true)
+		process.exit(1)
+	}
 
-		/**
-		 *
-		 */
+	/**
+	 *
+	 */
 
-		ids.forEach((id) => {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_CONNECTION_HAS_BEEN_CLOSED`, error)
-			this.___callbacks[id] &&
-			this.___callbacks[id](error)
+	___onSocketData (data) {
+		this.___protocol.read(data)
+	}
+
+	/**
+	 *
+	 */
+
+	___onProtocolMessage (message) {
+		const promise = this.___promises[message.id]
+		const resolve = promise && promise[0]
+		const reject = promise && promise[1]
+		message.error && reject && reject(message.error)
+		message.data && resolve && resolve(message.data)
+		delete this.___promises[message.id]
+	}
+
+	/**
+	 *
+	 */
+
+	async find (message = {}) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.find = `table`
+			___message.$db = db
+			___message.filter = message.filter || {}
+			___message.sort = message.sort || {}
+			___message.limit = message.limit || 999999999
+			___message.batchSize = message.limit || 999999999
+			___message.projection = message.projection || {}
+			___message.projection._id = 0
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
 		})
-
-		/**
-		 *
-		 */
-
-		this.___callbacks = {}
-		this.___reconnecting = true
-		this.___socket.___protocol.flush()
-
-		/**
-		 *
-		 */
-
-		setTimeout(() => this.___connect(), this.___timeout)
-
 	}
 
 	/**
 	 *
 	 */
 
-	___onConnect (error) {
-
-		/**
-		 *
-		 */
-
-		this.___protocol()
-		this.___reconnecting = false
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected) {
-
-			/**
-			 *
-			 */
-
-			this.___connected = true
-
-			/**
-			 *
-			 */
-
-			process.nextTick(() => this.emit(`ready`, error))
-
-		}
-
+	async insert (message = {}) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.insert = `table`
+			___message.$db = db
+			___message.documents = []
+			___message.documents.push(message)
+			___message.ordered = true
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	___onData (chunk) {
-
-		/**
-		 *
-		 */
-
-		this.___socket.___protocol.read(chunk)
-
+	async update (message = {}) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.update = `table`
+			___message.$db = db
+			___message.updates = []
+			___message.updates[0] = {}
+			___message.updates[0].q = message.filter || {}
+			___message.updates[0].u = message.update || {}
+			___message.updates[0].multi = !!message.multi || false
+			___message.updates[0].upsert = !!message.upsert || false
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	___onError (error) {
-
-		/**
-		 *
-		 */
-
-		process.nextTick(() => this.emit(`error`, error))
-
-		/**
-		 *
-		 */
-
-		this.___reconnect(error)
-
+	async remove (message = {}) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.delete = `table`
+			___message.$db = db
+			___message.deletes = []
+			___message.deletes[0] = {}
+			___message.deletes[0].q = message.filter || {}
+			// limit -
+			// specify either a 0 to delete all matching documents,
+			// or 1 to delete a single document
+			___message.deletes[0].limit = !message.multi && 1 || 0
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	___onEnd (error) {
-
-		/**
-		 *
-		 */
-
-		process.nextTick(() => this.emit(`error`, error))
-
-		/**
-		 *
-		 */
-
-		this.___reconnect(error)
-
+	async aggregate (message = {}) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.aggregate = `table`
+			___message.$db = db
+			___message.pipeline = message.pipeline
+			___message.cursor = {}
+			___message.cursor.batchSize = message.limit || 999999999
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	___onMessage (message) {
-
-		/**
-		 *
-		 */
-
-		const callback = this.___callbacks[message.id]
-
-		/**
-		 *
-		 */
-
-		if (!callback) {
-			return
-		}
-
-		/**
-		 *
-		 */
-
-		delete this.___callbacks[message.id]
-
-		/**
-		 *
-		 */
-
-		callback(message.error, message.data)
-
+	async removeIndex (index) {
+		if (index.name === `_id_`) return
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.dropIndexes = `table`
+			___message.$db = db
+			___message.index = index.name
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	find (message, callback) {
-
-		/**
-		 *
-		 */
-
-		message = message || {}
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.find = `table`
-		___message.$db = db
-		___message.filter = message.filter || {}
-		___message.sort = message.sort || {}
-		___message.limit = message.limit || 999999999
-		___message.batchSize = message.limit || 999999999
-		___message.projection = message.projection || {}
-		___message.projection._id = 0
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, data)
-
+	async createIndex (index) {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			const fields = Object.keys(index.keys)
+			const indexKey = index.keys
+			const indexName = `${!index.options.unique && `ix` || `ux`}@${fields.join(`|`)}`
+			___message.createIndexes = `table`
+			___message.$db = db
+			___message.indexes = []
+			___message.indexes[0] = {}
+			___message.indexes[0].key = indexKey
+			___message.indexes[0].name = indexName
+			___message.indexes[0].unique = !!index.options.unique
+			___message.indexes[0].background = !!index.options.background
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 	/**
 	 *
 	 */
 
-	insert (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.insert = `table`
-		___message.$db = db
-		___message.documents = []
-		___message.documents.push(message)
-		___message.ordered = true
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error) => callback(error, message)
-
-	}
-
-	/**
-	 *
-	 */
-
-	update (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.update = `table`
-		___message.$db = db
-		___message.updates = []
-		___message.updates[0] = {}
-		___message.updates[0].q = message.filter || {}
-		___message.updates[0].u = message.update || {}
-		___message.updates[0].multi = !!message.multi || false
-		___message.updates[0].upsert = !!message.upsert || false
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok && data.n || 0)
-
-	}
-
-	/**
-	 *
-	 */
-
-	remove (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 * limit - specify either a 0 to delete all matching documents, or 1 to delete a single document
-		 */
-
-		___message.delete = `table`
-		___message.$db = db
-		___message.deletes = []
-		___message.deletes[0] = {}
-		___message.deletes[0].q = message.filter || {}
-		___message.deletes[0].limit = !message.multi && 1 || 0
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok && data.n || 0)
-
-	}
-
-	/**
-	 *
-	 */
-
-	aggregate (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.aggregate = `table`
-		___message.$db = db
-		___message.pipeline = message.pipeline
-		___message.cursor = {}
-		___message.cursor.batchSize = message.limit || 999999999
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data || null)
-
-	}
-
-	/**
-	 *
-	 */
-
-	removeIndex (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.dropIndexes = `table`
-		___message.$db = db
-		___message.index = message
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok || null)
-
-	}
-
-	/**
-	 *
-	 */
-
-	createIndex (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		const fields = Object.keys(message.keys)
-
-		/**
-		 *
-		 */
-
-		const indexKey = message.keys
-
-		/**
-		 *
-		 */
-
-		const indexName = `${!message.options.unique && `ix` || `ux`}@${fields.join(`|`)}`
-
-		/**
-		 *
-		 */
-
-		___message.createIndexes = `table`
-		___message.$db = db
-		___message.indexes = []
-		___message.indexes[0] = {}
-		___message.indexes[0].key = indexKey
-		___message.indexes[0].name = indexName
-		___message.indexes[0].unique = !!message.options.unique
-		___message.indexes[0].background = !!message.options.background
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.ok || null)
-
-	}
-
-	/**
-	 *
-	 */
-
-	getIndexes (message, callback) {
-
-		/**
-		 *
-		 */
-
-		if (!this.___connected || this.___reconnecting) {
-			const error = ___error(`jiu-jitsu-mongo`, `FAIL`, `MONGO_SOCKET_IS_NOT_READY`)
-			return callback(error)
-		}
-
-		/**
-		 *
-		 */
-
-		const id = this.___id++
-		const db = this.___endpoint.db
-
-		/**
-		 *
-		 */
-
-		const ___message = {}
-		const ___options = {}
-
-		/**
-		 *
-		 */
-
-		___message.listIndexes = `table`
-		___message.$db = db
-		___message.cursor = {}
-
-		/**
-		 *
-		 */
-
-		___options.id = id
-		___options.db = db
-
-		/**
-		 *
-		 */
-
-		const buffer = this.___socket.___protocol.write(___message, ___options)
-
-		/**
-		 *
-		 */
-
-		this.___socket.write(buffer)
-
-		/**
-		 *
-		 */
-
-		this.___callbacks[id] = (error, data) => callback(error, !error && data && data.filter((index) => index.name !== `_id_`).map((index) => index.name) || null)
-
+	async getIndexes () {
+		return await new Promise((resolve, reject) => {
+			const id = this.___id++
+			const db = this.___options.db
+			const ___message = {}
+			const ___options = {}
+			___message.listIndexes = `table`
+			___message.$db = db
+			___message.cursor = {}
+			___options.id = id
+			___options.db = db
+			const buffer = this.___protocol.write(___message, ___options)
+			this.___promises[id] = [resolve, reject]
+			this.___socket.write(buffer)
+		})
 	}
 
 }
